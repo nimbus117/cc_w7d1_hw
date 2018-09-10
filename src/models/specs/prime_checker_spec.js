7820b32aeb9e6654a2eee481bbc8b4a6ab3f268e
nimbus117 <mail@nimbus117.co.uk>
2018-09-10 15:49:51 +0100

    added more tests

--- src/models/specs/prime_checker_spec.js
+++ src/models/specs/prime_checker_spec.js
@@ -13,4 +13,16 @@ describe('Prime', function () {
     const actual = primeChecker.checkPrime(5);
     assert.strictEqual(actual, true);
   })
+  it('should not be prime', function () {
+    const actual = primeChecker.checkPrime(4);
+    assert.strictEqual(actual, false);
+  })
+  it('1 should not be prime', function () {
+    const actual = primeChecker.checkPrime(1);
+    assert.strictEqual(actual, false);
+  })
+  it('should be prime again', function () {
+    const actual = primeChecker.checkPrime(199);
+    assert.strictEqual(actual, true);
+  })
 })
